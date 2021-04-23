6a4f2b6ce5d84ba442c088c5ed9543d474ef1a37
eweagraff <weagraff.emily@gmail.com>
2021-04-22 21:14:35 -0700

    added for loop and if/else statement to change background color

--- script.js
+++ script.js
@@ -3,16 +3,26 @@ $("#date").html(today.format(`MMMM D, YYYY`));
 
 var hour = today.hour();
 var times = $("textarea");
+
 for (var i = 0; i < times.length; i++) {
   if (times[i].id < hour) {
-    times.setAttribute("class", "past");
+    times[i].classList.add("past");
   } else if (times[i].id == hour) {
-    times.setAttribute("class", "present");
+    times[i].classList.add("present");
   } else {
-    times.setAttribute("class", "future");
+    times[i].classList.add("future");
   }
 }
 
+//$("textarea")
+//.css("background", "#d3d3d3")
+//.filter(function(index)){
+// return index === 1 || (this).Attr("class") === "past";
+//})
+
+//function past() {
+//var past = (document.getElementById("9", "10", "11").className = "past");
+//}
 //change text area colors based on time past present future
 
 //if in the past add class past, if in present add class present,if in future add class future
