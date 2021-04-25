15889330ab0fcaae45090128eac3affca7213815
eweagraff <weagraff.emily@gmail.com>
2021-04-25 14:24:56 -0700

    added local/get storage options

--- script.js
+++ script.js
@@ -14,19 +14,13 @@ for (var i = 0; i < times.length; i++) {
   }
 }
 
-//$("textarea")
-//.css("background", "#d3d3d3")
-//.filter(function(index)){
-// return index === 1 || (this).Attr("class") === "past";
-//})
+$(".saveBtn").on("click", function () {
+  var saveEvent = $(this).siblings("textarea").val();
+  var time = $(this).parent(".timeblock").data("time");
+  localStorage.setItem(time, saveEvent);
+});
 
-//function past() {
-//var past = (document.getElementById("9", "10", "11").className = "past");
-//}
-//change text area colors based on time past present future
-
-//if in the past add class past, if in present add class present,if in future add class future
-
-//save to local storage when I click on save button (addeventlistener)
-//value is what is typed into the text areas (us the .val() method in jquery)
-//Need 9 different keys for each time slot
+$("textarea").each(function () {
+  var getTime = $(this).parent(".timeblock").data("time");
+  $(this).val(localStorage.getItem(getTime));
+});
